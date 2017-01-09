3c70b6fa18a520b669f2d6477d107df813320834
Clement Hussenot <chussenot@gmail.com>
2017-01-09 10:26:48 +0100

    Add more zsh plugins

--- oh-my-zsh/config.zsh
+++ oh-my-zsh/config.zsh
@@ -1,6 +1,6 @@
 # http://blog.ysmood.org/my-ys-terminal-theme/
 ZSH_THEME="ys"
 
-plugins=(git sudo aws tmux tmuxinator rails ruby gem aws bower bundler brew-cask brew capistrano coffee docker emacs git-extras github heroku lein node npm nyan osx pip pod python redis-cli vagrant)
+plugins=(git sudo aws tmux tmuxinator rails ruby gem aws bower bundler brew-cask brew capistrano coffee docker emacs git-extras github heroku lein node npm nyan osx pip pod python redis-cli vagrant terraform scala sbt z docker-compose)
 
 source $ZSH/oh-my-zsh.sh
