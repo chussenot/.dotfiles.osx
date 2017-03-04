a01541014ebb437b84fddccab15a44cda3ed2839
Clement Hussenot <chussenot@gmail.com>
2017-03-04 17:49:38 +0100

    Update the configuration

--- oh-my-zsh/config.zsh
+++ oh-my-zsh/config.zsh
@@ -4,6 +4,3 @@ ZSH_THEME="ys"
 plugins=(hub composer git sudo aws tmux tmuxinator rails ruby gem aws bower bundler brew-cask brew capistrano coffee docker emacs git-extras github heroku lein node npm nyan osx pip pod python redis-cli vagrant terraform scala sbt z docker-compose)
 
 source $ZSH/oh-my-zsh.sh
-
-export ZSH_TMUX_AUTOSTART=true
-export ZSH_TMUX_AUTOCONNECT=true
