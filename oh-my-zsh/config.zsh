d60dccf02f91287872a6d1f758b5081947d5047b
Clement Hussenot <chussenot@gmail.com>
2017-03-04 17:45:29 +0100

    Autostart TMUX and add more plugins

--- oh-my-zsh/config.zsh
+++ oh-my-zsh/config.zsh
@@ -1,6 +1,9 @@
 # http://blog.ysmood.org/my-ys-terminal-theme/
 ZSH_THEME="ys"
 
-plugins=(git sudo aws tmux tmuxinator rails ruby gem aws bower bundler brew-cask brew capistrano coffee docker emacs git-extras github heroku lein node npm nyan osx pip pod python redis-cli vagrant terraform scala sbt z docker-compose)
+plugins=(hub composer git sudo aws tmux tmuxinator rails ruby gem aws bower bundler brew-cask brew capistrano coffee docker emacs git-extras github heroku lein node npm nyan osx pip pod python redis-cli vagrant terraform scala sbt z docker-compose)
 
 source $ZSH/oh-my-zsh.sh
+
+export ZSH_TMUX_AUTOSTART=true
+export ZSH_TMUX_AUTOCONNECT=true
