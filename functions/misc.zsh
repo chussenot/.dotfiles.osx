e0217276cf9fbdff97fd8055c021c820cd237545
Clement Hussenot <chussenot@gmail.com>
2017-03-17 09:21:45 +0100

    Add some functions and aliases

--- functions/misc.zsh
+++ functions/misc.zsh
@@ -1,3 +1,5 @@
+# Another quick function to shorten something like this:
+# ps aux | grep PROCESS_NAME. (Usage: psa NAME)
 psa () {
   ps aux | grep $1
 }
@@ -5,4 +7,9 @@ psa () {
 myuptime () {
   uptime | awk '{ print "Uptime:", $3, $4, $5 }' | sed 's/,//g'
   return;
+}
+
+top-commands () {
+  history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' \
+  | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n15
 }
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,18 @@ __reload_dotfiles() {
   cd .
 }
 alias reload!='__reload_dotfiles'
+
+## create parent directories on demand
+alias mkdir='mkdir -pv'
+
+## get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+
+alias c='clear'
+
+## make you look all busy and fancy in the eyes of non-technical people.
+alias busy="cat /dev/urandom | hexdump -C | grep "ca fe""
+
+## nethack game
+alias nethack='telnet -e ^c nethack.alt.org'
