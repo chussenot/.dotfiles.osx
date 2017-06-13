cc3ba20e9a4db35efa7c9ff9989ed7d4c3a2bc4a
Clement Hussenot <chussenot@gmail.com>
2017-06-13 22:45:48 +0200

    Add more functions

--- functions/misc.zsh
+++ functions/misc.zsh
@@ -13,3 +13,22 @@ top-commands () {
   history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' \
   | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n15
 }
+
+vplay () {
+  go-peerflix --player vlc `cat $1`
+}
+
+# Start an HTTP server from a directory, optionally specifying the port
+function httpserver() {
+  local port="${1:-8000}";
+  sleep 1 && open "http://localhost:${port}/" &
+  # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
+  # And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
+  python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port";
+}
+
+# Expose it to the internet
+function httpserver-ngrok() {
+  local port="${1:-8000}";
+  ngrok http ${port}
+}
