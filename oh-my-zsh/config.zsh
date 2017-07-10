68fe1957e012190322204e9ebcab5993bab99905
Clement Hussenot <chussenot@gmail.com>
2017-07-10 22:45:22 +0200

    Add oh my zsh catimg plugin

--- oh-my-zsh/config.zsh
+++ oh-my-zsh/config.zsh
@@ -1,6 +1,6 @@
 # http://blog.ysmood.org/my-ys-terminal-theme/
 ZSH_THEME="ys"
 
-plugins=(hub composer git sudo tmux tmuxinator rails ruby gem aws bower bundler brew-cask brew capistrano coffee docker emacs git-extras github heroku lein node npm nyan osx pip pod python redis-cli vagrant terraform scala sbt z docker-compose common-aliases gradle mvn ng pass perl tig web-search)
+plugins=(hub composer git sudo tmux tmuxinator rails ruby gem aws bower bundler brew-cask brew capistrano coffee docker emacs git-extras github heroku lein node npm nyan osx pip pod python redis-cli vagrant terraform scala sbt z docker-compose gradle mvn ng pass perl tig web-search catimg)
 
 source $ZSH/oh-my-zsh.sh
