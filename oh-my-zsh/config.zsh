d29449ce9ae118692ef4197fbdba9c8d46961829
Clement Hussenot <chussenota@gmail.com>
2017-07-21 20:50:12 +0200

    Add the K, a Directory listings for zsh with git features!
    https://github.com/supercrabtree/k

--- oh-my-zsh/config.zsh
+++ oh-my-zsh/config.zsh
@@ -2,6 +2,6 @@
 ZSH_CUSTOM="$HOME/.dotfiles/oh-my-zsh/custom"
 ZSH_THEME="chussenot"
 
-plugins=(git sudo tmux tmuxinator ruby gem aws bower bundler brew-cask brew coffee docker git-extras lein node npm osx pip python terraform scala sbt z docker-compose gradle mvn ng pass perl tig)
+plugins=(k git sudo tmux tmuxinator ruby gem aws bower bundler brew-cask brew coffee docker git-extras lein node npm osx pip python terraform scala sbt z docker-compose gradle mvn ng pass perl tig)
 
 source $ZSH/oh-my-zsh.sh
