03c3dfbebc82ac41aa452aa71721ea1695cfce69
Clement Hussenot <chussenota@gmail.com>
2018-07-10 00:25:49 +0200

    Colorize some commands outputs

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -18,3 +18,27 @@ alias busy="cat /dev/urandom | hexdump -C | grep "ca fe""
 
 # enable aliases to be sudo’ed
 alias sudo='sudo '
+
+# Colorize
+# `grc` works great for this. It is installable with brew and colorizes a number
+# of terminal commands out of the box, diff being one of them. So...
+export GRC=/usr/local/bin/grc
+alias colourify="$GRC -es --colour=auto"
+alias configure='colourify ./configure'
+alias diff='colourify diff'
+alias make='colourify make'
+alias gcc='colourify gcc'
+alias g++='colourify g++'
+alias as='colourify as'
+alias gas='colourify gas'
+alias ld='colourify ld'
+alias netstat='colourify netstat'
+alias ping='colourify ping'
+alias traceroute='colourify /usr/sbin/traceroute'
+alias head='colourify head'
+alias tail='colourify tail'
+alias dig='colourify dig'
+alias mount='colourify mount'
+alias ps='colourify ps'
+alias mtr='colourify mtr'
+alias df='colourify df'
