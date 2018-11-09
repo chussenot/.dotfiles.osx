c387aa3e7b3431e018216a6d232e272377830f03
Clement Hussenot <chussenota@gmail.com>
2018-11-09 21:04:50 +0100

    More plugins

--- oh-my-zsh/config.zsh
+++ oh-my-zsh/config.zsh
@@ -2,6 +2,6 @@
 ZSH_CUSTOM="$HOME/.dotfiles/oh-my-zsh/custom"
 ZSH_THEME="chussenot"
 
-plugins=(zsh-reload rust k git sudo tmux tmuxinator ruby gem aws bower bundler brew-cask brew coffee docker git-extras lein node npm osx pip python terraform scala sbt z docker-compose gradle mvn ng pass perl tig)
+plugins=(zsh-reload rust k git sudo tmux tmuxinator ruby gem aws bower bundler brew-cask brew coffee docker git-extras lein node npm osx pip python terraform scala sbt z docker-compose gradle mvn ng pass perl tig helm minikube nmap nomad nvm perl ssh-agent vault yarn)
 
 source $ZSH/oh-my-zsh.sh
